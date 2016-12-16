1a24fec32f8cbe1230e063c06dc55886e0078362
Alena Bekrina <abekrina@gmail.com>
2016-12-16 09:32:21 -0800

    Work on getUserLocation in progress

--- src/main/java/com/whereis/service/UserLocationService.java
+++ src/main/java/com/whereis/service/UserLocationService.java
@@ -1,7 +1,24 @@
 package com.whereis.service;
 
-/**
- * Created by amakarova on 16/12/16.
- */
+import com.whereis.dao.UserLocationDao;
+import com.whereis.dao.UserLocationDaoImpl;
+import com.whereis.model.User;
+import com.whereis.model.UserLocation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service("userLocationService")
+@Transactional
 public class UserLocationService {
+    @Autowired
+    UserLocationDaoImpl userLocationDao;
+
+    public UserLocation getCurrentUserLocation(User user) {
+        return userLocationDao.getCurrentLocationOfUser(user);
+    }
+
+    public void saveUserLocation(UserLocation userLocation) {
+        userLocationDao.saveUserLocation(userLocation);
+    }
 }
