c319b09b375b6dc0bfe5f1edd28b5946d2e12cab
Alena Bekrina <abekrina@gmail.com>
2016-12-18 15:14:17 -0800

    Made app work without boot

--- src/main/java/com/whereis/Application.java
+++ src/main/java/com/whereis/Application.java
@@ -4,14 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = HibernateJpaAutoConfiguration.class)
 @EnableTransactionManagement(proxyTargetClass = true)
-public class WhereisApplication {
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(WhereisApplication.class, args);
+		ApplicationContext context =
+				new AnnotationConfigApplicationContext(Application.class);
+		MessagePrinter printer = context.getBean(MessagePrinter.class);
+		printer.printMessage();
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
 	}
 
 }
