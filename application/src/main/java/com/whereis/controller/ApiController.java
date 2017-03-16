b6d3a1e85a3ffa8abefa4e5cd07e8d5ad3ca6bf1
Alena Bekrina <abekrina@gmail.com>
2017-03-15 18:29:40 -0700

    added debug message

--- application/src/main/java/com/whereis/controller/ApiController.java
+++ application/src/main/java/com/whereis/controller/ApiController.java
@@ -141,6 +141,7 @@ public class ApiController extends AbstractController {
     @RequestMapping(value = "/getforcurrentuser/", method = RequestMethod.GET)
     public Set<Group> getGroups() {
         User currentUser = getCurrentUser();
+        logger.info("get groups ready to return");
         return currentUser.getGroups();
     }
 }
